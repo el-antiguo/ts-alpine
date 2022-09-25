b678e5d9a479a886ee7d58bdbb50bf737b4bbbe5
el_antiguo <codomodragon2@gmail.com>
2022-09-25 09:25:55 -0400

    [ts] add decorator for watch variables

---
+++ src/decorators/Watch.ts
@@ -0,0 +1,14 @@
+export function Watch(fire: any) {
+    return function (target: any, watched: string) {
+        const oldMethod = target.$watchers
+
+        Object.defineProperty(target, '$watchers', {
+            value: function (self: any) {
+                oldMethod?.(self)
+
+                self.$watch(watched, (value: any) => self[fire](value))
+            },
+            configurable: true
+        })
+    }
+}
