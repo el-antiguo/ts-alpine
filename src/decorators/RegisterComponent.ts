262947375125bfb9633f5bbebcaea22915c969bc
el_antiguo <codomodragon2@gmail.com>
2022-09-25 09:24:48 -0400

    [ts] add decorator for component registration

---
+++ src/decorators/RegisterComponent.ts
@@ -0,0 +1,49 @@
+import { ComponentInterface } from '../types'
+
+export function RegisterComponent() {
+    return function (target: any) {
+        const className: string = target.name
+
+        window[className] = function (attributes: {[key:string]:any}, ...arg: any[]) {
+            const instance = new target(...arg)
+
+            setAttributes(instance, attributes)
+            injectInitMethod(instance)
+            exposeMethods(instance)
+
+            return instance
+        }
+    }
+}
+
+function setAttributes(instance: ComponentInterface, attributes: {[key:string]:any}): void {
+    Object.assign(instance, attributes)
+}
+
+function injectInitMethod(instance: ComponentInterface) {
+    const oldInit = instance.init
+
+    Object.defineProperty(instance, 'init', {
+        value: function () {
+            const self = Object.getPrototypeOf(instance)
+
+            self.$watchers?.(this)
+            self.$eventListeners?.(this)
+
+            oldInit?.()
+        }
+    })
+}
+
+function exposeMethods(instance: any) {
+    const excludeMethods = ['constructor', 'init']
+    const methods = Reflect.ownKeys(Object.getPrototypeOf(instance))
+        .filter((key: any) => !key.startsWith('$')
+            && !excludeMethods.includes(key)
+        )
+    methods.forEach(key => {
+        Object.defineProperty(instance, key, {
+            value: instance[key],
+        })
+    });
+}
